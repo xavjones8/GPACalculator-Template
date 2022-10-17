a676925df89a60eef97e4d9839e6597769d09458
Xavier Jones <xdjones@ncsu.edu>
2022-10-17 17:59:39 -0400

    Creates Template

--- app/src/main/java/com/example/gpacalculator_master/SemesterActivity.kt
+++ app/src/main/java/com/example/gpacalculator_master/SemesterActivity.kt
@@ -28,6 +28,8 @@ class SemesterActivity : AppCompatActivity() {
 
         lblProjectedGPA.text = "Projected GPA: $gpa"
         lblCurrentGPA.text = "Current GPA: $gpa"
+
+
         val class_names = mutableListOf<ClassRowDetails>()
 
         //Populates list view with number of classes
@@ -35,37 +37,19 @@ class SemesterActivity : AppCompatActivity() {
             class_names.add(ClassRowDetails("Class $i", null, null))
         }
 
+        // TODO: A listview isn't the most efficient way of displaying lists of items in Android. Research recycler views and improve this design.
         val listView: ListView = findViewById(R.id.semester_list)
         val listAdapter = ClassListAdapter(this, class_names)
         listView.adapter = listAdapter
 
 
         btnCalculate.setOnClickListener() {
-            val classes = mutableListOf<ClassDetails>()
 
-            for(i in 0..listAdapter.count - 1) {
-                val row = listAdapter.getItem(i)
-                if(row == null || !row.isComplete) continue
+        }
 
-                try {
-                    classes.add(ClassDetails(row.hours?.toInt()!!, row.grade!!))
-                } catch( e: Exception) {
-                    val alertDialogBuilder = AlertDialog.Builder(this)
-                    alertDialogBuilder.setMessage("Each class' credit hours amount must be a number\nEach class grade must be in the form of [Letter][+/-/_] Ex. A+, C, D-")
-                    alertDialogBuilder.setTitle("Error")
-
-//                    Toast.makeText(this,
-//                        "Each class' credit hours amount must be a number\nEach class grade must be in the form of [Letter][+/-/_] Ex. A+, C, D-",
-//                        Toast.LENGTH_LONG).show()
-                    alertDialogBuilder.show()
-                    continue
-                }
-
-                val semester = SemesterDetails(classes)
-                val gpaCalculator = GPACalculator(gpa, currentHours, semester)
-                lblProjectedGPA.text = "Projected GPA ${gpaCalculator.toString()}"
-
-            }
+        //TODO: Implement the Clear all button!
+        btnClearForm.setOnClickListener {
+            Toast.makeText(this, "Complete this method!", Toast.LENGTH_SHORT).show()
         }
 
     }
